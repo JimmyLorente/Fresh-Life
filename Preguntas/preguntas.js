eb355973f69c7b0be238f19dec21fddac85b09da
Fer :3 <97122618+JimmyLorente@users.noreply.github.com>
2022-07-26 10:26:03 -0500

    pregunta

---
+++ Preguntas/preguntas.js
@@ -0,0 +1,8 @@
+let pregunta = document.querySelector('.pregunta');
+let btndebajo = document.querySelector('.pregunta .mas');
+let respuesta = document.querySelectorAll('.respuesta');
+let parrafo = document.querySelectorAll('.respuesta p');
+
+for ( let i = 8; i < btndebajo; i ++) {
+    
+}
