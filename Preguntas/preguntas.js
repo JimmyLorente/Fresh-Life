36aa057d2491a61fcbf51c20ee352dca69d6fa7e
Fer :3 <lorentef12@gmail.com>
2022-08-04 07:40:30 -0500

    nuevo

--- Preguntas/preguntas.js
+++ Preguntas/preguntas.js
@@ -1,8 +1,28 @@
 let pregunta = document.querySelector('.pregunta');
-let btndebajo = document.querySelector('.pregunta .mas');
+let btndebajo = document.querySelector('.pregunta .mas')
 let respuesta = document.querySelectorAll('.respuesta');
 let parrafo = document.querySelectorAll('.respuesta p');
 
-for ( let i = 8; i < btndebajo; i ++) {
+for ( let i = 0; i < btndebajo; i ++) {
     
+    let altoParrafo = parrafo[i].clientHeight;
+    let switchc = 0;
+
+    btndebajo[i].addEventListener('click', () =>{
+        if (switchc == 0) {
+            respuesta[i].style.height = `${altoParrafo}px`;
+            pregunta[i].style.marginBottom = '10px';
+            btndebajo[i].innerHTML = '<i>-</i>';
+            switchc ++;
+        }
+
+        else if ( switchc == 1 ) {
+
+            respuesta[i].style.height = `0`;
+            pregunta[i].style.marginBottom = '0';
+            btndebajo[i].innerHTML = '<i>+</i>';
+            switchc --;
+
+        }
+    })
 }
